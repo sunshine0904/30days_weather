8a7f1e32be0fd285abece9245ded2d54a523f953
sunshine0904 <sunshineboylei@gmail.com>
2019-10-07 22:48:08 +0800

    modify Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 获取当前时间之后15天的天气及当前时间之前的当月的天气
+依赖安装：
+pip install requests
+pip install bs4
+pip install lxml
+pip install xlwt
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-import random
-# PC端浏览器
+﻿import random
+#PC端浏览器
 user_agent_list = [
             "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
             "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
